32b25ee3da92c95fe7b72e6b6652f5403934d00e
Zinaida-Razinkova <68029159+Zinaida-Razinkova@users.noreply.github.com>
2022-02-14 13:02:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  const activeMenu = () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  }
+
+  menuBtnRef.addEventListener('click', activeMenu);
+
+
+  // owl-carousel
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
