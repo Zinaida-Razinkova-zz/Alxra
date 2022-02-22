ef72f9c413824375e3e4739766af5be5e099c02c
Zinaida-Razinkova <68029159+Zinaida-Razinkova@users.noreply.github.com>
2022-02-22 07:05:08 +0200

    services

---
+++ js/counters.js
@@ -0,0 +1,19 @@
+// id="animated-years" class="counter animated-counter"
+
+// const time = 1000;
+// const stepYears = 2;
+
+// function animatedYears(num) {
+//     let i = document.querySelector("#animated-years");
+//     n = 0;
+//     let t = Math.round(time / (num / stepYears));
+//     let interval = setInterval(() => {
+//         n = n + stepYears;
+//         if (n == num) {
+//             clearInterval(interval);
+//         }
+//         i.innerHTML = n;
+//     }, t)
+// }
+// animatedYears(20, 'animated-years');
+
