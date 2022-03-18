0800942e5440b9350b12bf7715a25165c1223e8c
Zinaida-Razinkova <68029159+Zinaida-Razinkova@users.noreply.github.com>
2022-03-18 12:45:32 +0200

    commit

--- js/counters.js
+++ js/counters.js
@@ -17,3 +17,32 @@
 // }
 // animatedYears(20, 'animated-years');
 
+
+
+// forms
+// const buttonForm = document.querySelector('#button-form');
+const dataForm = document.querySelector('.contacts-form');
+// console.dir(dataForm.elements.myselect.value);
+// let dataInput1 = document.querySelector('#i-1');
+// let dataInput2 = document.querySelector('#i-2');
+// let dataInput3 = document.querySelector('#i-3');
+// let dataText1 = document.querySelector('#t-1');
+
+// console.log(buttonForm);
+
+const getData = (event) => {
+    event.preventDefault();
+    console.dir(dataForm.elements.myselect.value);
+    // const name = dataInput1.value;
+    // const tel = dataInput2.value;
+    // const email = dataInput3.value;
+    // const text = dataText1.value;
+
+    // console.log(name);
+    // console.log(tel);
+    // console.log(email);
+    // console.log(text);
+}
+
+dataForm.addEventListener("submit", getData);
+// buttonForm.addEventListener("click", getData);
